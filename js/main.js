de9a9b034a36f57c1b04ab59c738b8d402acee80
klekovvlad <100075182+klekovvlad@users.noreply.github.com>
2022-09-11 15:43:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 const menuMobile = document.querySelector('.header__nav');
-const menuButton = document.querySelector('.header__logo');
+const menuButton = document.querySelector('.mobile__button');
 const menuLinks = document.querySelectorAll('.nav__link');
 
+
 menuButton.addEventListener('click', () => {
-    menuMobile.classList.toggle('header__nav-open');
-    menuButton.classList.toggle('close');
-});
+    menuButton.childNodes[3].classList.toggle('out');
+    menuButton.childNodes[1].classList.toggle('topline');
+    menuButton.childNodes[5].classList.toggle('bottomline');
+    menuButton.classList.toggle('fixed');
+    menuMobile.classList.toggle('mobile__menu-open');
+})
+
 
 menuLinks.forEach(elem => {
     elem.addEventListener('click', () => {
-        menuMobile.classList.toggle('header__nav-open');
-        menuButton.classList.toggle('close');
+        menuMobile.classList.toggle('mobile__menu-open');
+        menuButton.childNodes[3].classList.toggle('out');
+        menuButton.childNodes[1].classList.toggle('topline');
+        menuButton.childNodes[5].classList.toggle('bottomline');
+        menuButton.classList.toggle('fixed');
     });
 });
